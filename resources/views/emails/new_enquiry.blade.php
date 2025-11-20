37c3eee7aa91126526163f4c2a4c69d11c616cec
fawaz chuhaibar <fawaz.chuhaibar@adnec.ae>
2025-11-20 10:50:15 +0400

    testing

--- resources/views/emails/new_enquiry.blade.php
+++ resources/views/emails/new_enquiry.blade.php
@@ -13,7 +13,29 @@ Here are the details:
 {{ \App\Helpers\CountryHelper::getCountryName($item['value'], 'en') }}<br>
 @else
 <strong>{{ $item['display'] }}:</strong><br />
-{{ isset($item['value']) ? $item['value'] : '' }}<br>
+<?php
+try {
+    echo $item['value'];
+} catch (\Throwable $th) {
+    
+    foreach ($item['value'] as $key => $ii) {
+        try {
+            if (isset($ii['value']) && is_array($ii['value'])) {
+                echo implode(', ', $ii['value']);
+                echo '<br>';
+            } elseif (isset($ii['value'])) {
+                echo $ii['value'];
+                echo '<br>';
+            }
+        } catch (\Throwable $e) {
+            info('Error displaying array item: ' . $e->getMessage());
+            info('Array item data: ' . print_r($ii, true));
+        }
+    }
+}
+?>
+<br />
+<br />
 @endif
 @endforeach
 </x-mail::panel>
